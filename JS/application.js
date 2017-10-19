96cb1b20ca1d768bed9db08814f60666a29564c8
Nicholas Lee <nicholasle89@hotmail.com>
2017-10-19 02:25:14 -0700

    add flag function and grid propagation

--- JS/application.js
+++ JS/application.js
@@ -1,3 +1,94 @@
+class Queue {
+    constructor() {
+        this.q = [];
+    }
+    enqueue(obj){
+        this.q.push(obj);
+    };
+    dequeue(){
+        return this.q.shift();
+    };
+    isEmpty(){
+        return this.q.length === 0;
+    };
+}
+
+/*bfs implementation similar to https://www.khanacademy.org/computing/computer-science/algorithms/breadth-first-search/p/challenge-implement-breadth-first-search*/
+function bfs(graph, id) {
+    var bfsInfo = [];
+
+    //initialization
+    for(var i = 0; i < graph.length; i++) {
+        bfsInfo[i] = {
+            distance: null,
+            parent: null
+        }
+    }
+    bfsInfo[id].distance = 0;
+
+    var bfsQueue = new Queue();
+    bfsQueue.enqueue(id);
+    while(!bfsQueue.isEmpty()) {
+        var currentId = bfsQueue.dequeue();
+        for(var i = 0; i < graph[currentId].length; i++) {
+            var neighbour = graph[currentId][i];
+            if(bfsInfo[neighbour].distance === null) {
+                bfsInfo[neighbour].distance = bfsInfo[currentId].distance + 1;
+                bfsInfo[neighbour].parent = currentId;
+                bfsQueue.enqueue(neighbour);
+            }
+        }
+    }
+    return bfsInfo;
+}
+
+function filterEdgeGrid(i) {
+    //nw, n, ne, e, se, s, sw, w
+    var position = [i-9, i-8, i-7, i+1, i+9, i+8, i+7, i-1];
+    
+    //position filtering (around the edge)
+    //size of the board 8x8 so we use 8
+    if(i % 8 == 0) {
+        position[0] = -1;
+        position[7] = -1;
+        position[6] = -1;
+    }
+    if((i+1) % 8 == 0) {
+        position[2] = -1;
+        position[3] = -1;
+        position[4] = -1;
+    }
+    if(i >= 0 && i <= 7) {
+        position[0] = -1;
+        position[1] = -1;
+        position[2] = -1;
+    }
+    if(i >= 56 && i <= 63) {
+        position[6] = -1;
+        position[5] = -1;
+        position[4] = -1;
+    }
+    return position;
+}
+
+function findNeighbours() {
+    var listOfNeighboursArray = [];
+    for(var i = 0; i < 64; i++) {
+        var neighboursArray = [];
+        if($('#' + i).children().length == 0) {
+            var position = filterEdgeGrid(i);
+
+            for(var j = 0; j < 8; j++) {
+                if(position[j] >= 0 && ( $('#' + position[j]).children().length == 0 || $('#' + position[j]).children('p').length > 0)) {
+                    neighboursArray.push(position[j]);
+                }
+            }
+        }
+        listOfNeighboursArray.push(neighboursArray.sort(function(a, b){return a - b;}));
+    }
+    return listOfNeighboursArray;
+}
+
 $(document).ready(function() {
     var mines = [];
     
@@ -10,8 +101,7 @@ $(document).ready(function() {
     //generate boxes and append mines into div
     for(var i = 0; i < 64; i++) {
         var div = $('<div></div>');
-        $('.board').append(div).find('div:last').addClass('box');
-        $('.board').find('div:last').attr({
+        $('.board').append(div).find('div:last').attr({
             class: 'box',
             id: i
         });
@@ -31,33 +121,9 @@ $(document).ready(function() {
 
         //increment if we found 1 mine around the grid
         let counter = 0;
-        //nw, n, ne, e, se, s, sw, w
-        position = [i-9, i-8, i-7, i+1, i+9, i+8, i+7, i-1];
-
-        //position filtering (around the edge)
-        //size of the board 8x8 so we use 8
-        if(i % 8 == 0) {
-            position[0] = -1;
-            position[7] = -1;
-            position[6] = -1;
-        }
-        if((i+1) % 8 == 0) {
-            position[2] = -1;
-            position[3] = -1;
-            position[4] = -1;
-        }
-        if(i >= 0 && i <= 7) {
-            position[0] = -1;
-            position[1] = -1;
-            position[2] = -1;
-        }
-        if(i >= 56 && i <= 63) {
-            position[6] = -1;
-            position[5] = -1;
-            position[4] = -1;
-        }
+        var position = filterEdgeGrid(i);
             
-        //console.log(position);
+        //check how many bombs around the grid, then add it to counter
         for (var j = 0; j < 8; j++) {
             //console.log(i + ':' + position[j]);
             if(position[j] >= 0) {
@@ -72,10 +138,33 @@ $(document).ready(function() {
         }
     }
 
-    //user click the box
+    var neighboursList = findNeighbours();
+
+    //player click the box
     $('.board').on('click', '.box', function() {
         $(this).removeClass('box').addClass('opened-box');
         $(this).children().removeClass('hide-icon').addClass('show-icon');
+        if($(this).children().length == 0) {
+            var openList = bfs(neighboursList, this.id);
+            for(var i = 0; i < openList.length; i++) {
+                //console.log(openList[i].distance + ' ' + openList[i].parent);
+                if(openList[i].distance != null && $('#' + i).hasClass('box')) {
+                    $('#' + i).removeClass('box').addClass('opened-box');
+                    $('#' + i).children().removeClass('hide-icon').addClass('show-icon');
+                }
+            }
+        }
+        
+        //when one box is opened, it should propagate to related one
+        //for empty box, it should open all the empty one around it until it hit a numbered box
+        
+    });
+
+    //when player right click
+    $('.board').on('contextmenu', '.box', function() {
+        $(this).toggleClass('fa fa-flag fa-2x hide-icon');
+        //prevent default menu appear
+        return false;
     });
 
 });
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mineSweeper
-I have made my first minesweeper game! More features will be added soon.
+I have made my first minesweeper game!
+
+19 OCT 2017 Update
+1. Grid Propagation added
+    - When player click the empty grid, nearby empty grids will be opened untill it hit a number grid
+2. Flag icon
+    - Right click the grid will toggle the Flag icon
+3. Next pending update
+    - disable Flag icon toggling function on opened grid
+    - decide whether the Flagged upopen grid will be opened if the related empty grid is clicked
+    - better documentation
+    - potentially seperating the bfs algorithm from application.js to another js file
+4. Future Update
+    - adding timer
+    - alert player if stepping on the bomb, then showing option to reset the game
+    - show how many grids have been opened and left, also the total amount of bombs and used flags on the board
+    - slow animation when opening the grid to show the propagation of the empty grid
